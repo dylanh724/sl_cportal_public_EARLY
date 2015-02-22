fd7e27f6dfa87f79d97bcd6900001d60c25c3e0f
dylanh724 <dh@smartlaunch.com>
2015-02-23 01:00:48 +0800

    2/22 Naming for js files to captiveportal-xx (required), fixes, tweaks

--- sl_cportal/captiveportal-login.js
+++ sl_cportal/captiveportal-login.js
@@ -1,26 +1,28 @@
 /**
- * Created by Smartlaunch on 2/15/2015.
+ * Created by Dylan @ Smartlaunch on 2/15/2015.
  */
-// Error catching >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// 0: Error catching >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 $.ajaxSetup({
-    "error":function() { alert("error"); }
+    // Usually triggered by invalid server IP:Port
+    // (or if captiveportal-sl_ip.js not found)
+    "error":function() { alert("Unknown Error"); }
 });
 
-// Init >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// 1: Init >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 $(document).ready(function() {
-    // Fade in
+    // 1: Fade in
     $('#wrapper').fadeIn(1200);
 });
 
-// Globals >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// 2: Globals >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 var Prefix = "http://";
-var ServerIP = "localhost";
-var ServerPort = "7833";
+var ServerIP = server[0];   // local IP
+var ServerPort = server[1]; // RESTful port
 var ServerAddress = Prefix + ServerIP + ":" + ServerPort;
 
 // TODO: Find Server IP & port
 
-// Main >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// 3: Main >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 function tryLogin() {
     // Get User + pass (+voucher) from form
     var user = document.getElementById( 'auth_user' ).value;
@@ -33,6 +35,7 @@ function tryLogin() {
     }
 }
 
+// 4: Validate form >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 function initValidate(_user, _pass, _voucher, _voucherLogin) {
     if (_voucherLogin) {
         // Check for invalid voucher (ignore user+pass)
@@ -56,28 +59,49 @@ function initValidate(_user, _pass, _voucher, _voucherLogin) {
     }
 }
 
+// 5: Set error on fail >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 function setError(e, msg){
     // Trigger error
-    $.error(msg + " @ " + e);
+    $.error(msg + " @ " + e)
 }
 
+// 6: Success: Finally login the user >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 function login(user, pass) {
     // http://localhost:7833/cportal/login?username=dylan&password=asdf
     console.log("User '" + user + "' attempting login @ " + ServerAddress + "..");
     var request = ServerAddress + "/cportal/login?username=" + user + "&password=" + pass;
-    console.log("GET (Boolean) >> " + ServerAddress + "/cportal/login?username=" + user + "&password=***");
+    console.log("Attempting GET (Boolean) >> " + ServerAddress + "/cportal/login?username=" + user + "&password=***");
     $.getJSON(request, function (result) {
         if (result) {
             $(' <div id="dialog" title="Successful Login">Success! Logging in..</div>').dialog();
-            console.log("Success! Logging in..");
-            document.getElementById("submit").onclick = finalEvents();
+            console.log("Successfully logged into SL");
+            finalEvents();
         } else {
-            $(' <div id="dialog" title="Failed Login">Invalid Credentials<br>(or unavailable WiFi slot)</div>').dialog();
+            $(' <div id="dialog" title="Failed Login">Invalid Credentials<br>(or unavailable WiFi slot)</div>').dialog({
+                modal: true,
+                draggable: true,
+                resizable: false,
+            });
             console.log("Invalid credentials, or unavailable WiFi slot");
         }
     });
 }
 
+// 7: Final events after successful login (POST) >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 function finalEvents() {
-    // Post-login
+    // Final events after login success
+    console.log("Attempting final actions/POST..")
+    try {
+        // POST
+        var form = document.createElement('loginform');
+        form.setAttribute('method', 'post');
+        form.setAttribute('action', '$PORTAL_ACTION$');
+        form.style.display = 'hidden';
+        document.body.appendChild(form)
+        form.submit();
+        //$.post( "$PORTAL_ACTION$" );
+    }
+    catch(e) {
+        setError("POST", "Unknown Error");
+    }
 }
